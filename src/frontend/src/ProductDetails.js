91d3047bda7925dc74cdaeed41484434cb2b2e56
Brian Holt <btholt@gmail.com>
2018-10-22 14:24:22 -0700

    add inventory mod buttons

---
+++ src/frontend/src/ProductDetails.js
@@ -0,0 +1,135 @@
+import React from "react";
+
+class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inventoryMod: 0,
+      loading: false
+    };
+
+    this.increment = this.mod.bind(this, true);
+    this.decrement = this.mod.bind(this, false);
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.inventory !== this.props.inventory) {
+      this.setState({ inventoryMod: 0 });
+    }
+  }
+  mod(increment) {
+    this.setState({ loading: true });
+    fetch(
+      `http://localhost:5000/api/inventory/${this.props.id}/${
+        increment ? "increment" : "decrement"
+      }`,
+      {
+        method: "POST"
+      }
+    ).then(() => {
+      this.setState({
+        inventoryMod: increment
+          ? this.state.inventoryMod + 1
+          : this.state.inventoryMod - 1,
+        loading: false
+      });
+    }, console.error);
+  }
+  render() {
+    const {
+      name,
+      supplierName,
+      inventory,
+      sku,
+      price,
+      shortDescription,
+      longDescription,
+      unitDescription,
+      dimensions,
+      weightInPounds,
+      reorderAmount,
+      status,
+      location,
+      productType,
+      digital
+    } = this.props;
+    const { inventoryMod, loading } = this.state;
+    return (
+      <div className="details">
+        <div className="details-main">
+          <div>
+            <h1>{name}</h1>
+            <h2>{supplierName}</h2>
+          </div>
+          <div>
+            <h1 className="inventory">
+              {inventory ? inventory + inventoryMod : ""}
+            </h1>
+            <div
+              className={`details-buttons${
+                loading || !inventory ? " loading" : ""
+              }`}
+            >
+              <button
+                disabled={loading || !inventory}
+                onClick={this.increment}
+                className="plus mod-button"
+              >
+                +
+              </button>
+              <button
+                disabled={loading || !inventory}
+                onClick={this.decrement}
+                className="minus mod-button"
+              >
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <strong>SKU:</strong> {sku}
+            </li>
+            <li>
+              <strong>Price:</strong> {price}
+            </li>
+            <li>
+              <strong>Digital:</strong> {digital ? "True" : "False"}
+            </li>
+            <li>
+              <strong>Dimensions:</strong> {dimensions}
+            </li>
+            <li>
+              <strong>Weight (lbs):</strong> {weightInPounds}
+            </li>
+            <li>
+              <strong>Reorder Amount:</strong> {reorderAmount}
+            </li>
+            <li>
+              <strong>Status:</strong> {status}
+            </li>
+            <li>
+              <strong>Location:</strong> {location}
+            </li>
+            <li>
+              <strong>Product Type:</strong> {productType}
+            </li>
+            <li>
+              <strong>Unit Description:</strong> {unitDescription}
+            </li>
+            <li>
+              <strong>Short Description:</strong> {shortDescription}
+            </li>
+            <li>
+              <strong>Long Description:</strong> {longDescription}
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductDetails;
--- src/frontend/src/ProductTable.js
+++ src/frontend/src/ProductTable.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Column, Table, AutoSizer } from "react-virtualized";
+import Modal from "./Modal";
+import ProductDetails from "./ProductDetails";
 
 class ProductTable extends React.Component {
   constructor(props) {
@@ -7,42 +9,63 @@ class ProductTable extends React.Component {
     this.state = {
       rows: [],
       start: 1,
-      stop: 10
+      stop: 10,
+      selectedRow: null
     };
-
-    this.interval = null;
 
     this.modRowsShowing = this.modRowsShowing.bind(this);
     this.getInventory = this.getInventory.bind(this);
+    this.handleRowClick = this.handleRowClick.bind(this);
+    this.handleModalClick = this.handleModalClick.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
   componentDidMount() {
     fetch("http://localhost:8000/api/products")
       .then(data => data.json())
       .then(({ items }) => {
         this.setState({ rows: items });
-        this.interval = setInterval(this.getInventory, 5000);
+        this.interval = Date.now() + 2000;
+        requestAnimationFrame(this.getInventory);
       });
   }
   getInventory() {
+    if (this.interval > Date.now()) {
+      requestAnimationFrame(this.getInventory);
+      return;
+    }
+
     const nums = Array.from({ length: this.state.stop - this.state.start })
-      .map((_, index) => index + this.state.start)
+      .map((_, index) => index + this.state.start + 1)
       .join(",");
     fetch(`http://localhost:5000/api/inventory?skus=${nums}`)
       .then(data => data.json())
       .then(skus => {
-        let newRows = Array.from(this.state.rows);
-        skus.forEach(sku => {
-          newRows[sku.sku - 1].inventory = sku.quantity;
-        });
+        for (let i = 0; i < skus.length; i++) {
+          // cloning a list of 100,000 is bad
+          this.state.rows[+skus[i].sku - 1].inventory = skus[i].quantity; // eslint-disable-line
+        }
 
-        this.setState({ rows: newRows });
-      });
+        this.forceUpdate();
+        this.interval = Date.now() + 5000;
+        requestAnimationFrame(this.getInventory);
+      }, console.error);
   }
   modRowsShowing({ overscanStartIndex, overscanStopIndex }) {
     this.setState({
       start: overscanStartIndex,
       stop: overscanStopIndex
     });
+  }
+  handleRowClick(rowEvent) {
+    this.setState({ selectedRow: rowEvent.rowData });
+  }
+  handleModalClick(e) {
+    if (e.target.id === "modal-interior") {
+      this.closeModal();
+    }
+  }
+  closeModal() {
+    this.setState({ selectedRow: null });
   }
   render() {
     return (
@@ -52,21 +75,42 @@ class ProductTable extends React.Component {
             <Table
               width={height}
               height={width}
-              headerHeight={20}
+              headerHeight={60}
               rowHeight={60}
               rowCount={this.state.rows.length}
               rowGetter={({ index }) => this.state.rows[index]}
               onRowsRendered={this.modRowsShowing}
+              rowClassName={({ index }) => (index % 2 ? "row-even" : "row-odd")}
+              headerClassName="row-header"
+              onRowClick={this.handleRowClick}
             >
               <Column width={80} label="ID" dataKey="id" />
               <Column width={300} label="Name" dataKey="name" />
               <Column width={300} label="SKU" dataKey="sku" />
               <Column width={100} label="Price" dataKey="price" />
               <Column width={200} label="Supplier" dataKey="supplierName" />
-              <Column width={200} label="Inventory" dataKey={"inventory"} />
+              <Column width={200} label="Inventory" dataKey="inventory" />
             </Table>
           )}
         </AutoSizer>
+        {!this.state.selectedRow ? null : (
+          <Modal>
+            <div
+              role="none"
+              onClick={this.handleModalClick}
+              id="modal-interior"
+            >
+              <div>
+                <ProductDetails {...this.state.selectedRow} />
+                <div className="buttons">
+                  <button className="exit-button" onClick={this.closeModal}>
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     );
   }
