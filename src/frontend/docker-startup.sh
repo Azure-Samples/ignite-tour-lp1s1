2ccf2e9c4520db4940f9c10f4a707df9f8dc9bb3
Anthony Chu <antchu@microsoft.com>
2018-11-04 16:16:38 -0800

    Use caddy to serve frontend

--- src/frontend/docker-startup.sh
+++ src/frontend/docker-startup.sh
@@ -1,3 +1,8 @@
-npm run build
-cd /frontend/dist
-/frontend/node_modules/.bin/http-server
+cd dist
+PRODUCT_SERVICE_BASE_URL=${PRODUCT_SERVICE_BASE_URL//\//\\\/}
+grep -rl '%%PRODUCT_SERVICE_BASE_URL%%' . | xargs sed -i "s/%%PRODUCT_SERVICE_BASE_URL%%/$PRODUCT_SERVICE_BASE_URL/g"
+
+INVENTORY_SERVICE_BASE_URL=${INVENTORY_SERVICE_BASE_URL//\//\\\/}
+grep -rl '%%INVENTORY_SERVICE_BASE_URL%%' . | xargs sed -i "s/%%INVENTORY_SERVICE_BASE_URL%%/$INVENTORY_SERVICE_BASE_URL/g"
+
+caddy -port $PORT
